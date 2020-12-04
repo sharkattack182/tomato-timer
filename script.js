b003251552806e3008a3496b3b15636ee8802cf3
Matthew Courtney <matthewdc182@gmail.com>
2020-12-04 00:42:52 -0500

    formatted min and seconds just to make sure there was always a two didgit value

--- script.js
+++ script.js
@@ -12,8 +12,30 @@ var totalSeconds = 0;
 var secondsElapsed = 0;
 var interval;
 
+function formatMinutes() {
+    var secondsLeft = totalSeconds - secondsElapsed;
+    var minutesLeft = Math.floor(secondsLeft / 60);
+    var formattedMinutes;
 
+    if(minutesLeft < 10) {
+        formattedMinutes = "0" + minutesLeft;
+    } else {
+        formattedMinutes = minutesLeft;
+    }
 
+    return formattedMinutes;
+}
 
+function formatSeconds() {
+    var secondsLeft = (totalSeconds - secondsElapsed) % 60;
+    var formattedSeconds;
+
+    if(secondsLeft < 10) {
+        formattedSeconds = "0" + secondsLeft;
+    } else {
+        formattedSeconds = secondsLeft;
+    }
+    return formattedSeconds;
+}
 
 
